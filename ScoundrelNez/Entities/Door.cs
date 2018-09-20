6d82e95f4df31f9f032e166bb46426ffd2887ae8
David Arneson <urvaius@gmail.com>
2018-09-20 16:13:15 -0700

    not sure how to implement door yet.

---
+++ ScoundrelNez/Entities/Door.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Nez.Sprites;
+using Microsoft.Xna.Framework.Graphics;
+using Nez.Textures;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using Nez.Tiled;
+using Nez;
+
+namespace ScoundrelNez.Entities
+{
+    public class Door : Component, ITriggerListener, IUpdatable
+    {
+
+        BoxCollider _boxCollider;
+        bool IUpdatable.enabled => throw new System.NotImplementedException();
+
+        int IUpdatable.updateOrder => throw new System.NotImplementedException();
+
+
+        public override void onAddedToEntity()
+        {
+            base.onAddedToEntity();
+            var texture = entity.scene.content.Load<Texture2D>(Content.Textures.powerup);
+            _boxCollider = entity.getComponent<BoxCollider>();
+
+
+        }
+        public void onTriggerEnter(Collider other, Collider local)
+        {
+            Debug.log("triggerEnter:{0}", other.entity.name);
+        }
+
+        public void onTriggerExit(Collider other, Collider local)
+        {
+            Debug.log("triggerEnter:{0}", other.entity.name);
+        }
+
+        void IUpdatable.update()
+        {
+            
+        }
+    }
+}
